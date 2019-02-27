329314f6d663ecdc3e2c4bae6dbedcd81326daea
Viktor Marutyan <viktor-marutyan@mail.ru>
2019-02-27 18:34:35 +0400

    k

--- src/Products.js
+++ src/Products.js
@@ -73,7 +73,7 @@ const products = [
         imageSrc: "https://cdn.vox-cdn.com/thumbor/HtsyeSUY5qMjCaPLtMIO1IoQ21Y=/0x0:2880x1350/1200x800/filters:focal(1102x445:1562x905)/cdn.vox-cdn.com/uploads/chorus_image/image/56659171/model_s_001.0.jpg",
         price: 20000,
         name: "Tesla",
-        description: "Sed ut perspiciatis unde omnis isgjhte natus error sit voluptatem accusantium doloremque laudantium.",
+        description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
     },
 ]
 
